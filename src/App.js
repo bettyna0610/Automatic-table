4b67209de207832a4277adb5a6e1f27fedbeb7e3
Bettina <bettyna0610@gmail.com>
2020-02-06 15:29:18 +0100

    Final changes in data

--- src/App.js
+++ src/App.js
@@ -1,49 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import {offers} from './Components/data';
+import { offers } from './Data/data';
 import Table from './Components/table.js';
 
 
 class App extends React.Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     table:offers
-   }
-   //alert( this.state.table );
-   //alert( "TA = " + this.state.table );
-   //alert( "TA2 = " + Object.keys( this.state.table ).length );
-   //this.tableAdd();
-   window.setTimeout( this.tableAdd.bind( this ), 5000 );
- } 
+  // Getting data from offers object to the initial state. 
 
- tableAdd = () => {
-  //alert( "TA = " + this.state.table );
-  //alert( "TA2 = " + Object.keys( this.state.table ).length );
-  let l = Object.keys( this.state.table ).length;
-  let dd2 = new Date();
-  offers[ l ] = {"id":l, "date": new Date(dd2.setMinutes(dd2.getMinutes() + l*10)).toDateString(), "amount": (Math.random()*10).toFixed(2),
-    "bid": 9.5 + Math.round(Math.random())};
-  this.setState( { table: offers } );
-  window.setTimeout( this.tableAdd.bind( this ), 5000 );
+  constructor(props) {
+    super(props);
+    this.state = {
+      table: offers
+    }
+  }
+// After the table was loaded, the setInterval function calls tabbleAdd adding new rows in every 5 seconds. 
+  componentDidMount = () => {
+    setInterval(() => {
+      this.tableAdd();
+    }, 5000);
+  }
 
- }
+  tableAdd = () => {
+    // I created offersCopy not to mutate the original data.
+    // The newRow is the new element of the object as the last element of the initial table is length-1.I used toDateString to delete timezone from the date object.
+    // I reduced amount to 2 decimals, and rounded bid to have more transperent data.
+    // It also updates the state.
+    const newRow = Object.keys(this.state.table).length;
+    const dd2 = new Date();
+    const offersCopy = offers;
 
-render() {
-  console.log(offers)
-  return (
-    <div>
-      <table>
-        <tbody>
-      <Table table={this.state.table}/>
-      </tbody>
-      </table>
-      
-    </div>
-  )
-}
+    offersCopy[newRow] = {
+      "id": newRow, "date": new Date(dd2.setMinutes(dd2.getMinutes() + newRow * 10)).toDateString(), "amount": (Math.random() * 10).toFixed(2),
+      "bid": 9.5 + Math.round(Math.random())
+    };
+    this.setState({ table: offersCopy });
+  }
+
+  render() {
+    console.log(offers)
+    return (
+      //The table component gets the offers data from the state.
+      <div>
+        <table>
+          <tbody>
+            <Table table={this.state.table} />
+          </tbody>
+        </table>
+
+      </div>
+    )
+  }
 
 }
 export default App;
--- src/Components/table.js
+++ src/Components/table.js
@@ -1,54 +1,36 @@
 import React from 'react';
-import {BootstrapTable, 
-    TableHeaderColumn} from 'react-bootstrap-table';
+import {
+  BootstrapTable,
+  TableHeaderColumn
+} from 'react-bootstrap-table';
 
-/*
-const Table = (props) => {
 
-    
-    const table = props.table
-    console.log(table[0].date)
-    return Object.keys(table).map((obj,i) => 
-      { const { id, date, amount, bid } = table[obj]
-      
-      return (
-        <tr key={id}>
-        <td>{id}</td>
-         <td>{date.toString()}</td>
-         <td>{amount}</td>
-      <td>{bid}</td>
-      </tr>
-      )
-    })
-}*/
 
 const Table = (props) => {
 
+  //Getting the values of offers object as Bootstrap table only accepts array.
+  const values = Object.values(props.table);
 
-    
+  return (
+    <div className="table">
+      <BootstrapTable striped bordered hover data={values}  >
+        <TableHeaderColumn dataAlign="center" width={'50px'} isKey dataField='id'>
+          ID
+            </TableHeaderColumn>
+        <TableHeaderColumn dataAlign="center" width={'140px'} dataField='date'>
+          Date
+            </TableHeaderColumn>
+        <TableHeaderColumn dataAlign="center" width={'95px'} dataField='amount'>
+          Amount
+            </TableHeaderColumn>
+        <TableHeaderColumn width={'55px'} dataAlign="center" dataField='bid'>
+          Bid
+            </TableHeaderColumn>
+      </BootstrapTable>
+    </div>
+  );
+}
 
-        const values = Object.values(props.table);
 
-      return (
-        <div className="table">
-          <BootstrapTable   striped bordered hover  data={values}  >
-            <TableHeaderColumn dataAlign="center" width={'50px'}isKey dataField='id'>
-              ID
-            </TableHeaderColumn>
-            <TableHeaderColumn dataAlign="center" width={'140px'} dataField='date'>
-              Date
-            </TableHeaderColumn>
-            <TableHeaderColumn dataAlign="center" width={'95px'} dataField='amount'>
-            Amount
-            </TableHeaderColumn>
-            <TableHeaderColumn width={'55px'} dataAlign="center" dataField='bid'>
-            Bid
-            </TableHeaderColumn>
-          </BootstrapTable>
-        </div>
-      );
-    }
-  
-       
 
 export default Table;
